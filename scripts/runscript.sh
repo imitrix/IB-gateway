b716afecd6cd7fb2881a39f8e9ee9cbfacb12ea8
Alex Pressl <pressl.alex@imitrix.de>
2021-02-25 14:43:21 +0100

    v

--- scripts/runscript.sh
+++ scripts/runscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xvfb-daemon-run /opt/ibc/scripts/DisplayBannerAndLaunch.sh &
+xvfb-daemon-run /opt/ibc/scripts/displaybannerandlaunch.sh &
 # Tail latest in log dir
 sleep 1
 tail -f $(find $LOG_PATH -maxdepth 1 -type f -printf "%T@ %p\n" | sort -n | tail -n 1 | cut -d' ' -f 2-) &
